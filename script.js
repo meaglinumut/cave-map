37b2a6d36a05f659a426f65f2a39835b965f3e98
meaglinumut <meaglinumut@gmail.com>
2026-05-22 09:21:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,25 @@ fetch("map.svg")
   });
 const zoomSlider = document.getElementById("zoom-slider");
 const zoomValue = document.getElementById("zoom-value");
+const mapContainer = document.getElementById("map-container");
 const mapWrapper = document.getElementById("map-wrapper");
 
 zoomSlider.addEventListener("input", () => {
-  const zoom = zoomSlider.value;
-  zoomValue.textContent = zoom + "%";
-  mapWrapper.style.transform = `scale(${zoom / 100})`;
+  const oldZoom = Number(mapWrapper.dataset.zoom || 1);
+  const newZoom = Number(zoomSlider.value) / 100;
+
+  const centerX = mapContainer.scrollLeft + mapContainer.clientWidth / 2;
+  const centerY = mapContainer.scrollTop + mapContainer.clientHeight / 2;
+
+  const mapX = centerX / oldZoom;
+  const mapY = centerY / oldZoom;
+
+  mapWrapper.style.transform = `scale(${newZoom})`;
   mapWrapper.style.transformOrigin = "top left";
+  mapWrapper.dataset.zoom = newZoom;
+
+  mapContainer.scrollLeft = mapX * newZoom - mapContainer.clientWidth / 2;
+  mapContainer.scrollTop = mapY * newZoom - mapContainer.clientHeight / 2;
+
+  zoomValue.textContent = Math.round(newZoom * 100) + "%";
 });
